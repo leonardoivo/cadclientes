859f7021247ff4e7fe539ca86d692042345860ae
Henrique Rosa <henrique.rosa@meta.com.br>
2021-09-06 09:34:45 -0300

    Adicionada validação ao criar empresa parceira e testes da consulta e busca por id

---
+++ tests/Tiradentes.CobrancaAtiva.Unit/EmpresaParceiraTestes/ConsultaEmpresaParceira.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using Tiradentes.CobrancaAtiva.Api.Controllers;
+using Tiradentes.CobrancaAtiva.Application.AutoMapper;
+using Tiradentes.CobrancaAtiva.Application.QueryParams;
+using Tiradentes.CobrancaAtiva.Domain.Interfaces;
+using Tiradentes.CobrancaAtiva.Infrastructure.Context;
+using Tiradentes.CobrancaAtiva.Infrastructure.Repositories;
+using Tiradentes.CobrancaAtiva.Services.Interfaces;
+using Tiradentes.CobrancaAtiva.Services.Services;
+
+namespace Tiradentes.CobrancaAtiva.Unit.EmpresaParceiraTestes
+{
+    public class ConsultaEmpresaParceira
+    {
+        private EmpresaParceiraController _controller;
+        private CobrancaAtivaDbContext _context;
+        private IEmpresaParceiraService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            DbContextOptions<CobrancaAtivaDbContext> optionsContext =
+                new DbContextOptionsBuilder<CobrancaAtivaDbContext>()
+                    .UseInMemoryDatabase("CobrancaAtivaTests")
+                    .Options;
+            _context = new CobrancaAtivaDbContext(optionsContext);
+            IEmpresaParceiraRepository repository = new EmpresaParceiraRepository(_context);
+            IMapper mapper = new Mapper(AutoMapperSetup.RegisterMappings());
+            IEmpresaParceiraService service = new EmpresaParceiraService(repository, mapper);
+            _controller = new EmpresaParceiraController(service);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _service?.Dispose();
+        }
+
+        [Test]
+        [TestCase(TestName = "Teste Consultar Empresa Parceira",
+                    Description = "Teste usando um banco vazio e não enviado dados de paginação")]
+        public async Task TesteBuscarTodos()
+        {
+            var t = await _controller.Buscar(new ConsultaEmpresaParceiraQueryParam());
+            Assert.AreEqual(t.Value.Items.Count, 0);
+            Assert.AreEqual(t.Value.TotalItems, 0);
+            Assert.AreEqual(t.Value.TotalPaginas, 0);
+            Assert.AreEqual(t.Value.PaginaAtual, 1);
+            Assert.AreEqual(t.Value.TamanhoPagina, 10);
+        }
+    }
+}
