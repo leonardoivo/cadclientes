d5dee0d6028307235a9c04b1a124d24db139b7de
Leonardo Ivo Neves da Silva <leonardo.neves@meta.com.br>
2021-12-15 15:54:59 -0300

    Criação de controoler da janela de conflitos

---
+++ src/Tiradentes.CobrancaAtiva.Api/Controllers/ConflitoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tiradentes.CobrancaAtiva.Application.ViewModels.Conflito;
+using Tiradentes.CobrancaAtiva.Services.Interfaces;
+
+namespace Tiradentes.CobrancaAtiva.Api.Controllers
+{
+    [ApiController]
+    [Route("Conflito")]
+    public class ConflitoController : ControllerBase
+    {
+        private readonly IConflitoService _service;
+
+        public ConflitoController(IConflitoService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<ConflitoViewModel>>> Buscar() =>
+             Ok(await _service.Buscar());
+    }
+}
