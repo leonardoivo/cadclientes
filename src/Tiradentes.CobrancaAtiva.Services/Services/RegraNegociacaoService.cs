b63ed80e131204b087f41e3ed2e59b03e275461a
Alessandro <alessandrodahlke47@tutanota.com>
2021-12-15 15:31:36 -0300

    Continuação implementação conflito de regra.

--- src/Tiradentes.CobrancaAtiva.Services/Services/RegraNegociacaoService.cs
+++ src/Tiradentes.CobrancaAtiva.Services/Services/RegraNegociacaoService.cs
@@ -68,13 +68,6 @@ namespace Tiradentes.CobrancaAtiva.Services.Services
 
             var model = _map.Map<RegraNegociacaoModel>(viewModel);
 
-            var conflito = _repositorio.VerificarRegraConflitante(model);
-
-            if (conflito != null)
-            {
-                return _map.Map<RegraNegociacaoViewModel>(conflito);
-            }
-
             await _repositorio.Criar(model);
 
             return _map.Map<RegraNegociacaoViewModel>(model);
@@ -102,13 +95,13 @@ namespace Tiradentes.CobrancaAtiva.Services.Services
             return _map.Map<RegraNegociacaoViewModel>(model);
         }
 
-        public async Task<BuscaRegraNegociacao> VerificarRegraConflitante(RegraNegociacaoViewModel viewModel)
+        public async Task<RegraNegociacaoViewModel> VerificarRegraConflitante(RegraNegociacaoViewModel viewModel)
         {
             var model = _map.Map<RegraNegociacaoModel>(viewModel);
 
             var conflito = await _repositorio.VerificarRegraConflitante(model);
 
-            return _map.Map<BuscaRegraNegociacao>(conflito);
+            return _map.Map<RegraNegociacaoViewModel>(conflito);
         }
     }
 }
