77e64ed3f0c616bdb890183ac3aece673975bd10
Lucas Lira <l.lirajw@hotmail.com>
2022-01-13 17:55:17 -0300

    fix: hasnokey

---
+++ src/Tiradentes.CobrancaAtiva.Infrastructure/Mappings/MatriculaAlunoExisteMapping.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tiradentes.CobrancaAtiva.Domain.Models;
+
+namespace Tiradentes.CobrancaAtiva.Infrastructure.Mappings
+{
+    public class MatriculaAlunoExisteMapping : IEntityTypeConfiguration<MatriculaAlunoExisteModel>
+    {
+        public void Configure(EntityTypeBuilder<MatriculaAlunoExisteModel> builder)
+        {
+            builder.HasNoKey();
+        }
+    }
+}
---
+++ src/Tiradentes.CobrancaAtiva.Infrastructure/Mappings/ParcelaPagaAlunoInstituicaoMapping.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using Tiradentes.CobrancaAtiva.Domain.Models;
+
+namespace Tiradentes.CobrancaAtiva.Infrastructure.Mappings
+{
+    public class ParcelaPagaAlunoInstituicaoMapping : IEntityTypeConfiguration<ParcelaPagaAlunoInstituicaoModel>
+    {
+        public void Configure(EntityTypeBuilder<ParcelaPagaAlunoInstituicaoModel> builder)
+        {
+            builder.HasNoKey();
+        }
+    }
+}
