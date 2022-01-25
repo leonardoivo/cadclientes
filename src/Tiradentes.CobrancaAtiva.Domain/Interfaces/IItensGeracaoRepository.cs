3b99014f4394d5fbc0d8fbfa2f43be238726efb3
Lucas Lira <l.lirajw@hotmail.com>
2022-01-25 16:07:54 -0300

    correção nome

--- src/Tiradentes.CobrancaAtiva.Domain/Interfaces/IItensGeracaoRepository.cs
+++ src/Tiradentes.CobrancaAtiva.Domain/Interfaces/IItensGeracaoRepository.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Threading.Tasks;
 using Tiradentes.CobrancaAtiva.Domain.Models;
 
 namespace Tiradentes.CobrancaAtiva.Domain.Interfaces
 {
-    public interface IItensGeracaoRepository
+    public interface IItensGeracaoRepository : IDisposable
     {
+        public bool ExisteMatricula(string cnpjEmpresa, decimal matricula, decimal periodo, int parcela);
+
+        public DateTime ObterDataEnvio(string cnpjEmpresa, decimal matricula, decimal periodo, int parcela);
         Task Criar(ItensGeracaoModel model);
+
     }
 }
