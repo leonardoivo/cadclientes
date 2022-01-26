5d0388b74c642888fb3c929cff8d50185ccd0eea
Henrique Rosa <henrique.rosa@meta.com.br>
2022-01-26 17:26:47 -0300

    iniciado ajuste

--- src/Tiradentes.CobrancaAtiva.Application/ViewModels/Cobranca/RespostaViewModel.cs
+++ src/Tiradentes.CobrancaAtiva.Application/ViewModels/Cobranca/RespostaViewModel.cs
@@ -84,8 +84,8 @@ namespace Tiradentes.CobrancaAtiva.Application.ViewModels.Cobranca
         public DateTime? DataBaixa { get; set; }
         public decimal ValorPago { get; set; }
         public string TipoPagamento { get; set; }
-
         public bool Integrado { get; set; }
+        public int Erro { get; set; } 
 
         public decimal ObterPeriodo()
         {
