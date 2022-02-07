333c5c55f2f1d4d1972899e7614270dda23a3df9
Henrique Rosa <henrique.rosa@meta.com.br>
2022-02-06 22:10:51 -0300

    adicionado consultas mais genericas

--- src/Tiradentes.CobrancaAtiva.Application/ViewModels/BaixaPagamento/ItensBaixaPagamentoViewModel.cs
+++ src/Tiradentes.CobrancaAtiva.Application/ViewModels/BaixaPagamento/ItensBaixaPagamentoViewModel.cs
@@ -5,7 +5,7 @@ namespace Tiradentes.CobrancaAtiva.Application.ViewModels.BaixaPagamento
     public class ItensBaixaPagamentoViewModel
     {
         public int Tipo { get; set; } 
-        public long NumeroLinha { get; set; }
+        public long? NumeroLinha { get; set; }
         public long Matricula { get; set; }
         public string NomeAluno { get; set; }
         public long NumeroAcordo { get; set; }
