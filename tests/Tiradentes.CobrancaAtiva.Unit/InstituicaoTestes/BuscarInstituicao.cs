27effe52054b2082ccd80c6b06ed02a1f485b5b6
Silvio Viana <silvio.viana@meta.com.br>
2022-02-16 18:30:00 -0300

    fix: Testes adicionados

---
+++ tests/Tiradentes.CobrancaAtiva.Unit/InstituicaoTestes/BuscarInstituicao.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Tiradentes.CobrancaAtiva.Application.AutoMapper;
+using Tiradentes.CobrancaAtiva.Domain.Interfaces;
+using Tiradentes.CobrancaAtiva.Domain.Models;
+using Tiradentes.CobrancaAtiva.Infrastructure.Context;
+using Tiradentes.CobrancaAtiva.Infrastructure.Repositories;
+using Tiradentes.CobrancaAtiva.Services.Interfaces;
+using Tiradentes.CobrancaAtiva.Services.Services;
+using System.Threading.Tasks;
+
+namespace Tiradentes.CobrancaAtiva.Unit.InstituicaoTestes
+{
+    public class BuscarInstituicao
+    {
+        private CobrancaAtivaDbContext _context;
+        private IInstituicaoService _service;
+        private InstituicaoModel _model;
+        private IMapper _mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            DbContextOptions<CobrancaAtivaDbContext> optionsContext =
+                new DbContextOptionsBuilder<CobrancaAtivaDbContext>()
+                    .UseInMemoryDatabase("InstituicaoTestes")
+                    .Options;
+
+            _context = new CobrancaAtivaDbContext(optionsContext);
+            IInstituicaoRepository repository = new InstituicaoRepository(_context);
+            _mapper = new Mapper(AutoMapperSetup.RegisterMappings());
+            _service = new InstituicaoService(repository, _mapper);
+
+            _model = new InstituicaoModel
+            {
+                Instituicao = "UNIT"
+            };
+
+            if(_context.Instituicao.CountAsync().Result == 0)
+            {
+                _context.Instituicao.Add(_model);
+                _context.SaveChanges();
+            }
+            
+            _context.ChangeTracker.Clear();
+        }
+
+        [Test]
+        [TestCase(TestName = "Teste Buscar intituição válido",
+                   Description = "Teste Buscar intituição no Banco")]
+        public async Task TesteBuscarInstituicaoValido()
+        {
+            var Instituicoes = await _service.Buscar();
+
+            Assert.AreEqual(1, Instituicoes.Count);
+        }
+    }
+}
---
+++ tests/Tiradentes.CobrancaAtiva.Unit/ModalidadeTestes/BuscarPorCodigo.cs
@@ -0,0 +1,69 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Tiradentes.CobrancaAtiva.Application.AutoMapper;
+using Tiradentes.CobrancaAtiva.Domain.Interfaces;
+using Tiradentes.CobrancaAtiva.Domain.Models;
+using Tiradentes.CobrancaAtiva.Infrastructure.Context;
+using Tiradentes.CobrancaAtiva.Infrastructure.Repositories;
+using Tiradentes.CobrancaAtiva.Services.Interfaces;
+using Tiradentes.CobrancaAtiva.Services.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace Tiradentes.CobrancaAtiva.Unit.ModalidadeTestes
+{
+    public class BuscarPorCodigo
+    {
+        private CobrancaAtivaDbContext _context;
+        private IModalidadeService _service;
+        private ModalidadeModel _model;
+        private IMapper _mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            DbContextOptions<CobrancaAtivaDbContext> optionsContext =
+                new DbContextOptionsBuilder<CobrancaAtivaDbContext>()
+                    .UseInMemoryDatabase("ModalidadeTestes")
+                    .Options;
+            _context = new CobrancaAtivaDbContext(optionsContext);
+
+            IdAlunoRepository idAlunoRepository = new IdAlunoRepository(_context);
+            ParcelaTituloRepository parcelaTituloRepository = new ParcelaTituloRepository(_context);
+            IModalidadeRepository repository = new ModalidadeRepository(_context);
+            _mapper = new Mapper(AutoMapperSetup.RegisterMappings());
+            _service = new ModalidadeService(repository, _mapper);
+
+            _model = new ModalidadeModel
+            {
+                Modalidade = "Presencial",        
+                CodigoMagister = "123"
+            };
+
+            if(_context.Modalidade.CountAsync().Result == 0)
+            {
+                _context.Modalidade.Add(_model);
+                _context.SaveChanges();
+            }
+            
+            _context.ChangeTracker.Clear();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _service?.Dispose();
+        }
+
+        [Test]
+        [TestCase(TestName = "Teste Buscar Modalidade Por Codigo válido",
+                   Description = "Teste Buscar Modalidade Por Codigo no Banco")]
+        public async Task TesteBuscarPorCodigoValido()
+        {
+            var Modalidade = await _service.BuscarPorCodigo(_model.CodigoMagister);
+
+            Assert.AreEqual(_model.Id, Modalidade.Id);
+        }
+    }
+}
